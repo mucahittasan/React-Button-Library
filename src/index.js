3717f71f061d1bbc5d7482890d7295b453b132cb
Mucahit Tasan <mucahittasan00@gmail.com>
2022-04-02 17:19:20 +0300

    button hiyerarsisi duzenlendi

--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
 import React from 'react'
-import styles from './styles.module.css'
+import './styles.css'
 
 export const Button = (props) => {
 
-if(props.type === 'primary'){
-  return <button className={styles.primary} {...props}>{props.children}</button>
-}else if(props.type === ''){
-  return <button className={styles.default} {...props}>{props.children}</button>
-}else if(props.type === 'dashed'){
-  return <button className={styles.dashed} {...props}>{props.children}</button>
-}else if(props.type === 'text'){
-  return <button className={styles.text} {...props}>{props.children}</button>
-}else if(props.type === 'link'){
-  return <button className={styles.link} {...props}>{props.children}</button>
-}
+  return <button className={props.type} {...props}>{props.children}</button>
 
 }
