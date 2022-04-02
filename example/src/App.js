b8a57d13d1674e43c6a973d8ce39314cf2391e5b
Mucahit Tasan <mucahittasan00@gmail.com>
2022-04-02 17:05:12 +0300

    first commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
-import { ExampleComponent } from 'stealui'
+import { ExampleComponent, Button } from 'stealui'
 import 'stealui/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return(
+    <>
+      <Button type='primary'>Primary</Button>
+      <Button type=''>Default</Button>
+      <Button type='dashed'>Dashed</Button>
+      <Button type='text'>Text</Button>
+      <Button type='link'>Link</Button>
+    </>
+  ) 
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const Button = (props) => {
+
+if(props.type === 'primary'){
+  return <button className={styles.primary} {...props}>{props.children}</button>
+}else if(props.type === ''){
+  return <button className={styles.default} {...props}>{props.children}</button>
+}else if(props.type === 'dashed'){
+  return <button className={styles.dashed} {...props}>{props.children}</button>
+}else if(props.type === 'text'){
+  return <button className={styles.text} {...props}>{props.children}</button>
+}else if(props.type === 'link'){
+  return <button className={styles.link} {...props}>{props.children}</button>
+}
+
 }
